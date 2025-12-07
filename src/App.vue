087822064e38304181669b138ab7e931a161b8d7
Bagastia <atmajabagastia@192.168.1.25>
2025-12-07 22:57:10 +0800

    done2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="bg-white dark:bg-slate-800 min-h-screen font-sans transition-colors duration-300">
+  <div class="bg-white dark:bg-slate-800 min-h-screen font-sans transition-colors duration-300 w-full relative">
     <Navbar />
     <router-view />
     <FooterSection />
-    
   </div>
 </template>
 
 <script setup>
-import Navbar from './components/Navbar.vue'
-import FooterSection from './components/FooterSection.vue'
+  import Navbar from './components/Navbar.vue';
+  import FooterSection from './components/FooterSection.vue';
 </script>
